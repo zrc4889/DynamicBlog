a2d644099c72538fcb1f086011e930a9d63607f8
zrc4889 <zrc4889@163.com>
2024-11-17 10:30:35 +0800

    11

--- 1mail.py
+++ 1mail.py
@@ -1,7 +1,13 @@
 import smtplib
-import os
+import datetime
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+
+def text():
+    today = datetime.date.today()
+    past_date = datetime.date(2025, 1, 1)
+    delta = today - past_date
+    return '距离新年还有 ' + delta + ' 天，加油！'
 
 # 发件人和收件人信息
 sender_email = "zrc_4889@163.com"
@@ -13,10 +19,10 @@ password = "ZASRS37tuwxerHBZ"
 message = MIMEMultipart()
 message["From"] = sender_email
 message["To"] = receiver_email
-message["Subject"] = "欢迎回家！"
+message["Subject"] = text()
 
 # 添加邮件正文
-message.attach(MIMEText("正文。", "plain"))
+message.attach(MIMEText("111", "plain"))
 
 # 连接到SMTP服务器
 with smtplib.SMTP("smtp.163.com", 25) as server:
