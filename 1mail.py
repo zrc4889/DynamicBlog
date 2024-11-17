5d8a5fb9295560fe647cc6d53f8e18b0cfb65c2c
zrc4889 <zrc4889@163.com>
2024-11-17 10:57:58 +0800

    1

--- 1mail.py
+++ 1mail.py
@@ -3,31 +3,40 @@ import datetime
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-def text():
-    today = datetime.date.today()
-    past_date = datetime.date(2025, 1, 1)
-    delta = today - past_date
-    return '距离新年还有 ' + delta.days + ' 天，加油！'
-
 # 发件人和收件人信息
 sender_email = "zrc_4889@163.com"
 receiver_email = "zrc4889@163.com"
 password = "ZASRS37tuwxerHBZ"
 # password = os.environ['PASSWORD']
 
-# 创建邮件
-message = MIMEMultipart()
-message["From"] = sender_email
-message["To"] = receiver_email
-message["Subject"] = text()
+def send(s):
+    # 创建邮件
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = s
+    
+    # 添加邮件正文
+    message.attach(MIMEText("111", "plain"))
+    
+    # 连接到SMTP服务器
+    with smtplib.SMTP("smtp.163.com", 25) as server:
+        server.starttls()
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+    
+    # print("邮件已发送成功！")
 
-# 添加邮件正文
-message.attach(MIMEText("111", "plain"))
+today = datetime.date.today()
+s = ''
+data = {'2025新年': datetime.date(2025, 1, 1),
+        # '蛇年春节': datetime.date(2025, 1, 29),
+        '2025三战上岸': datetime.date(2025, 7, 1)
+        # '2027 全国高考': datetime.date(2027, 6, 7)
+    }
+s = s + '加油 CharonTree！'
+for key, val in data.items():
+    s = s + '距离' + str(key) + '还有' + str((val - today).days) + '天。'
+    
+send(s)
 
-# 连接到SMTP服务器
-with smtplib.SMTP("smtp.163.com", 25) as server:
-    server.starttls()
-    server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message.as_string())
-
-# print("邮件已发送成功！")
