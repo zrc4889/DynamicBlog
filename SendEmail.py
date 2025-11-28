3b1060a7826d0f640d45cdd6c69fdd371c5af775
CharonTree <zrc4889@163.com>
2025-11-28 22:59:25 +0800

    Update SendEmail.py

--- SendEmail.py
+++ SendEmail.py
@@ -7,7 +7,6 @@ from email.mime.multipart import MIMEMultipart
 sender_email = "zrc_4889@163.com"
 receiver_email = "zrc4889@163.com"
 password = "ZASRS37tuwxerHBZ"
-# password = os.environ['PASSWORD']
 
 def send(s):
     # 创建邮件
@@ -36,4 +35,5 @@ for key, val in data.items():
     s = s + '中午好，今天是编号' + str((val - today).days) + '天。'
 day = (val - today).days
 send(s)
+send('自证是最大的陷阱，诬告你的人就是要让你陷入自证陷阱，因为她知道你什么都没干，你自证她也不会承认，所以千万不要陷入自证陷阱。最好的方法是谁主张谁举证，把锅甩到别人身上，还有就是千万不要道歉或者认错，人做错了事才会道歉和认错，你什么都没做，道什么歉认什么错。不要被别人的咄咄逼人的态度压住，自己没做错事就昂首挺胸拿出气势来压倒别人，就和视频里表现的一样。')
 
